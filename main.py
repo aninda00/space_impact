0481630671a1d2cf20e0e9e163e65dbbf1678fe4
aninda00 <aninda.agd@gmail.com>
2026-09-08 18:37:11 +0600

    icon

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import pygame
 import sys
 from core.game import Game
+import ctypes
 
+# Force Windows to register custom taskbar icon
+if sys.platform == 'win32':
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID('spaceimpact.remastered.v1')
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((1080, 720), pygame.RESIZABLE)
+    try:
+        game_icon = pygame.image.load("assets/icon.png")
+        pygame.display.set_icon(game_icon)
+    except Exception as e:
+         print(f"Icon load failed: {e}")
     pygame.display.set_caption("Space Impact — Remastered")
     Game(screen).run()
 
